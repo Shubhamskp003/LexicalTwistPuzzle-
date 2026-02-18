18d2cfbb56cd674c7b5194a4cd716685dd53e6fa
Shubhamskp003 <123391547+Shubhamskp003@users.noreply.github.com>
2026-02-18 15:41:50 +0530

    [Shubham] Added: UC3:Validate Single Word Constraint

--- LexicalTwistPuzzle.java
+++ LexicalTwistPuzzle.java
@@ -1,29 +1,37 @@
 import java.util.Scanner;
 
 /**
- * LexicalTwistPuzzle UC1: Display Puzzle Title
  * LexicalTwistPuzzle UC2: Accept Word Inputs
+ * LexicalTwistPuzzle UC3: Validate Single Word Constraint
  *
- * This class Display a static message indicating the purpose of the application.
  * This class Accept two words from the user for puzzle processing..
+ * This class Ensure that each input contains only one word.
  *
  * @Developer
- * @version 1.0
  * @version 2.0
+ * @version 3.0
  * */
 
 
-public class LexicalTwistPuzzle {
-    public static void main(String[] args) {
-        System.out.println("Lexical Twist Puzzle");
-        Scanner sc = new Scanner(System.in);
-
+@@ -17,9 +17,21 @@ public static void main(String[] args) {
         System.out.print("Enter the first word: ");
         String firstWord = sc.nextLine();
+
+        if(firstWord.trim().contains(" ")){
+        System.out.println(firstWord + " is not valid word.");
+        return;
+        }
 
         System.out.print("Enter the second word: ");
         String secondWord = sc.nextLine();
 
+        if(secondWord.trim().contains(" ")){
+        System.out.println(secondWord + " is not valid word.");
+        return;
+        }
+
+        System.out.println("Valid word: " + firstWord + " " + secondWord);
+
         sc.close();
-    }
-}
+        }
+        }
